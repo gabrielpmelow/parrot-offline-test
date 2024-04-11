b3121af29786513a2f700977b6fb99c304293e6f
Gabriel <119764622+gabrielpmelow@users.noreply.github.com>
2024-04-11 14:41:35 -0300

    folders like the "pyscript offline" section ask

---
+++ public/pyscript/core.js
@@ -0,0 +1,2 @@
+export{f as MPWorker,b as PyWorker,T as TYPES,g as config,e as hooks,s as stdlib,h as whenDefined}from"./core-2NwtOg82.js";
+//# sourceMappingURL=core.js.map
---
+++ public/pyscript/deprecations-manager-Dj-XJTYm.js
@@ -0,0 +1,2 @@
+import{e}from"./core-2NwtOg82.js";import{notify as o}from"./error-CYOIlKpM.js";function r(){const e=document.querySelectorAll("script");for(const o of e)s(o.src)}function s(e){/\/pyscript\.net\/latest/.test(e)&&o("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.")}e.main.onReady.add(r),e.main.onWorker.add(r);
+//# sourceMappingURL=deprecations-manager-Dj-XJTYm.js.map
